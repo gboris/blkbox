9eee9ff5d997d5f7e0124b5331d0d4c76e341727
Boris Guennewig <boris@bg-13pro-mid14.local>
2016-06-20 15:46:12 +1000

    minor lintr changes

--- R/blkbox.R
+++ R/blkbox.R
@@ -16,51 +16,51 @@
 #' @export
 blkbox <- function(data, labels, holdout, holdout.labels, ntrees, mTry, Kernel, Gamma, exclude){
 
-  startMem = pryr::mem_used()
-  startTime = Sys.time()
+  startMem <- pryr::mem_used()
+  startTime <- Sys.time()
 
-  if(!hasArg(data)){
+  if (!hasArg(data)){
     stop("Ensemble cannot run without data, provide data.frame of samples by features")
   }
 
-  if(!hasArg(labels)){
+  if (!hasArg(labels)){
     stop("Ensemble cannot run without class, provide to 'labels' parameter")
   } else {
-    if(length(levels(as.factor(labels))) > 2){
+    if (length(levels(as.factor(labels))) > 2){
       stop("blkbox does not support non-binary classification tasks")
     }
   }
 
   #class will appropraite the labels into a data frame
   #The data from feature selected data will not contain a response column and therefore will need to be bound
-  labels = as.numeric(factor(x = labels, labels = c(1,2)))
-  class = data.frame(y = (c(labels)))
-  class.data = cbind(data, class)
-  actual.label = data.frame(labels = class.data$y, row.names = rownames(class.data))
+  labels <- as.numeric(factor(x = labels, labels = c(1,2)))
+  class <- data.frame(y = (c(labels)))
+  class.data <- cbind(data, class)
+  actual.label <- data.frame(labels = class.data$y, row.names = rownames(class.data))
 
-  cv.train = class.data
-  classtr = data.frame(condition = factor(cv.train$y))
+  cv.train <- class.data
+  classtr <- data.frame(condition = factor(cv.train$y))
 
   #reduce the holdout set to the same features as decicided upon in feature selection
 
 
-  class_ho = data.frame(y = (c(holdout.labels)))
-  cv.test = holdout[, which(colnames(holdout) %in% colnames(data))]
-  cv.test = cbind(cv.test, class_ho)
-  classts = data.frame(condition = factor(holdout.labels))
+  class_ho <- data.frame(y = (c(holdout.labels)))
+  cv.test <- holdout[, which(colnames(holdout) %in% colnames(data))]
+  cv.test <- cbind(cv.test, class_ho)
+  classts <- data.frame(condition = factor(holdout.labels))
 
   #Creating lists for data storage
-  algorithm.importance = list()
-  algorithm.votes = list()
-  algorithm_list = list()
+  algorithm.importance <- list()
+  algorithm.votes <- list()
+  algorithm_list <- list()
 
-  if(!hasArg(exclude)){
+  if (!hasArg(exclude)){
     exclude = c(0)
   }
 
-  if(ncol(class.data) > 4001){
+  if (ncol(class.data) > 4001){
     tree.method = 1
-    if(1 %in% exclude == FALSE){
+    if (1 %in% exclude == FALSE){
       if (!requireNamespace("bigrf", quietly = TRUE)) {
         message("The bigrf package is not installed.\nWithout this analysis with randomforest for large numbers of features is very slow.\nInstall it via 'devtools::install_github('aloysius-lim/bigrf')'")
         tree.method = 0
@@ -72,22 +72,22 @@ blkbox <- function(data, labels, holdout, holdout.labels, ntrees, mTry, Kernel, 
 
 
 
-  if(hasArg(Kernel)){
+  if (hasArg(Kernel)){
     svm.kernel = Kernel
   } else {
     svm.kernel = "linear"
   }
-  if(hasArg(Gamma)){
+  if (hasArg(Gamma)){
     svm.gamma = Gamma
   } else {
     svm.gamma = 1/(ncol(class.data)-1)
   }
-  if(hasArg(m.try)){
+  if (hasArg(m.try)){
     m.try = mTry
   } else {
     m.try = round(sqrt(ncol(class.data)))
   }
-  if(hasArg(ntrees)){
+  if (hasArg(ntrees)){
     nTrees = ntrees
   } else {
     nTrees = 501
@@ -95,47 +95,47 @@ blkbox <- function(data, labels, holdout, holdout.labels, ntrees, mTry, Kernel, 
 
 
 
-  if(1 %in% exclude == FALSE){
-    if(tree.method > 0){
+  if (1 %in% exclude == FALSE){
+    if ( tree.method > 0){
       algorithm_list[["randomforest"]] = .BB_BRF(cv.train = cv.train, cv.test = cv.test, classtr = classtr, classts = classts, m.try = m.try, nTrees = nTrees)
     } else {
       algorithm_list[["randomforest"]] = .BB_RF(cv.train = cv.train, cv.test = cv.test, classtr = classtr, m.try = m.try, nTrees = nTrees)
     }
   }
-  if(2 %in% exclude == FALSE){
+  if (2 %in% exclude == FALSE){
     algorithm_list[["kknn"]] = .BB_KKNN(cv.train = cv.train, cv.test = cv.test)
   }
-  if(3 %in% exclude == FALSE){
+  if (3 %in% exclude == FALSE){
     algorithm_list[["bartmachine"]] = .BB_BARTM(cv.train = cv.train, cv.test = cv.test, nTrees = nTrees)
   }
-  if(4 %in% exclude == FALSE){
+  if (4 %in% exclude == FALSE){
     algorithm_list[["party"]] = .BB_PARTY(cv.train = cv.train, cv.test = cv.test, m.try = m.try, nTrees = nTrees)
   }
-  if(5 %in% exclude == FALSE){
+  if (5 %in% exclude == FALSE){
     algorithm_list[["GLM"]] = .BB_GLM(cv.train = cv.train, cv.test = cv.test)
   }
-  if(6 %in% exclude == FALSE){
+  if (6 %in% exclude == FALSE){
     algorithm_list[["PamR"]] = .BB_PAM(cv.train = cv.train, cv.test = cv.test)
   }
-  if(7 %in% exclude == FALSE){
+  if (7 %in% exclude == FALSE){
     algorithm_list[["nnet"]] = .BB_NNET(cv.train = cv.train, cv.test = cv.test)
   }
-  if(8 %in% exclude == FALSE){
+  if (8 %in% exclude == FALSE){
     algorithm_list[["SVM"]] = .BB_SVM(cv.train = cv.train, cv.test = cv.test, classtr = classtr, svm.kernel = svm.kernel, svm.gamma = svm.gamma)
   }
 
 
-  for(q in 1:length(algorithm_list)){
-    if(names(algorithm_list)[q] != "kknn"){
+  for (q in 1:length(algorithm_list)){
+    if (names(algorithm_list)[q] != "kknn"){
       algorithm.importance[[names(algorithm_list)[q]]] = algorithm_list[[q]]$IMP
     }
     algorithm.votes[[names(algorithm_list)[q]]] = as.matrix(algorithm_list[[q]]$VOTE)
   }
 
-  endTime = Sys.time()
-  endMem = pryr::mem_used()
-  diffMem = endMem - startMem
-  elapsedTime = endTime - startTime
+  endTime <- Sys.time()
+  endMem <- pryr::mem_used()
+  diffMem <- endMem - startMem
+  elapsedTime <- endTime - startTime
 
   return(list("algorithm.votes" = algorithm.votes, "algorithm.importance" = algorithm.importance, benchmarks = list("time" = elapsedTime, "memory.used" = diffMem), "input.data" = list("Data" = class.data ,"labels" = classts)))
 
