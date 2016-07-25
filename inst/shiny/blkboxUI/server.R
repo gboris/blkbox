6293211d40601fd6c07c67cb86d5be63d888904e
Zachary Davies <zacdav@users.noreply.github.com>
2016-07-25 23:19:06 +1000

    Update server.R

--- inst/shiny/blkboxUI/server.R
+++ inst/shiny/blkboxUI/server.R
@@ -1,9 +1,11 @@
 library(shiny)
+library(shinyjs)
+
 
 fetch_obj_names <- function(class, compliment = F){
   which_obj_names <- sapply(as.list(.GlobalEnv), class = class, function(x, class){
     ifelse(class %in% class(x), TRUE, FALSE)
-    })
+  })
   # compliment provides the opposite results if TRUE
   if(compliment == F){
     obj_names = names(as.list(.GlobalEnv))[which_obj_names]
@@ -25,73 +27,99 @@ shinyServer(function(input, output) {
   output$label_selection = renderUI({
     selectInput("label_selection", label = "Labels", choices = fetch_obj_names("data.frame", T), width = "240px")
   })
-
+  
   # Run Model -----------------------------------------------------------------------------
   observeEvent(input$submit_model, {
     print("Model Submitted")
     print(code_str)
     blkbox_model <- eval(parse(text = code_str))
   })
-
+  
   # Get Code Output -----------------------------------------------------------------------
-
   observe({
-
+    
     if (input$model_type == 1){
-    # Training & Testing ------------------------------------------------------------------
+      # Training & Testing ------------------------------------------------------------------
       code_str <<- paste0("blkbox(data = Partition(",
-                     input$data_selection, ", ", input$label_selection, ", ",
-                     "size = ", input$partition_slider,
-                     ifelse(is.na(input$partition_seed) || input$partition_seed_ask == F, "", paste0(", seed = ", input$partition_seed)),")",
-                     ifelse(input$ntree != 500 && input$options == T, paste0(",\n       ntrees = ", input$ntree), ""),
-                     ifelse(!is.na(input$mtry) && input$options == T, paste0(",\n       mTry = ", input$mtry), ""),
-                     ifelse(input$svm_kernel != "linear" && input$options == T, paste0(",\n       Kernel = ", "'", input$svm_kernel, "'"), ""),
-                     ifelse(!is.na(input$svm_gamma) && input$options == T, paste0(",\n       Gamma = ", input$svm_gamma), ""),
-                     ifelse(!is.na(input$max_depth) && input$options == T, paste0(",\n       max.depth = ", input$max_depth), ""),
-                     ifelse(input$exclude_alg == "0" && length(input$exclude_alg) == 1, "",
-                            paste0(",\n       exclude = c(", toString(paste0("'", input$exclude_alg[-which(input$exclude_alg %in% "0")])),")")),
-                     ifelse(is.na(input$seed) || input$seed_ask == F, "", paste0(",\n       seed = ", input$seed)),")")
-
+                          input$data_selection, ", ", input$label_selection, ", ",
+                          "size = ", input$partition_slider,
+                          ifelse(is.na(input$partition_seed) || input$partition_seed_ask == F, "", paste0(", seed = ", input$partition_seed)),")",
+                          ifelse(input$ntree != 500 && input$options == T, paste0(",\n       ntrees = ", input$ntree), ""),
+                          ifelse(!is.na(input$mtry) && input$options == T, paste0(",\n       mTry = ", input$mtry), ""),
+                          ifelse(input$svm_kernel != "linear" && input$options == T, paste0(",\n       Kernel = ", "'", input$svm_kernel, "'"), ""),
+                          ifelse(!is.na(input$svm_gamma) && input$options == T, paste0(",\n       Gamma = ", input$svm_gamma), ""),
+                          ifelse(!is.na(input$max_depth) && input$options == T, paste0(",\n       max.depth = ", input$max_depth), ""),
+                          ifelse(input$exclude_alg == "0" && length(input$exclude_alg) == 1, "",
+                                 paste0(",\n       exclude = c(", toString(paste0("'", input$exclude_alg[-which(input$exclude_alg %in% "0")])),")")),
+                          ifelse(is.na(input$seed) || input$seed_ask == F, "", paste0(",\n       seed = ", input$seed)),")")
+      
     } else if (input$model_type == 2){
-    # Cross-fold Validation ---------------------------------------------------------------
+      # Cross-fold Validation ---------------------------------------------------------------
       code_str <<- paste0("blkboxCV(data = ", input$data_selection,
-                     ", labels = ", input$label_selection,
-                     ifelse(input$fold_number == 10, "", paste0(",\n       folds = ", input$fold_number)),
-                     ifelse(input$cv_repeat_number == 1, "", paste0(",\n       repeats = ", input$cv_repeat_number)),
-                     ifelse(input$cv_auc_slider == 0.5, "", paste0(",\n       AUC = ", input$cv_auc_slider)),
-                     ifelse(input$cv_method == "GLM", "", paste0(",\n       Method = ", "'", input$cv_method, "'")),
-                     ifelse(input$ntree != 500 && input$options == T, paste0(",\n       ntrees = ", input$ntree), ""),
-                     ifelse(!is.na(input$mtry) && input$options == T, paste0(",\n       mTry = ", input$mtry), ""),
-                     ifelse(input$svm_kernel != "linear" && input$options == T, paste0(",\n       Kernel = ", "'", input$svm_kernel, "'"), ""),
-                     ifelse(!is.na(input$svm_gamma) && input$options == T, paste0(",\n       Gamma = ", input$svm_gamma), ""),
-                     ifelse(!is.na(input$max_depth) && input$options == T, paste0(",\n       max.depth = ", input$max_depth), ""),
-                     ifelse(input$exclude_alg == "0" && length(input$exclude_alg) == 1, "",
-                            paste0(",\n       exclude = c(", toString(paste0("'", input$exclude_alg[-which(input$exclude_alg %in% "0")])),")")),
-                     ifelse(is.na(input$seed) || input$seed_ask == F, "", paste0(",\n       seed = ", input$seed)),")")
-
+                          ", labels = ", input$label_selection,
+                          ifelse(input$fold_number == 10, "", paste0(",\n       folds = ", input$fold_number)),
+                          ifelse(input$cv_repeat_number == 1, "", paste0(",\n       repeats = ", input$cv_repeat_number)),
+                          ifelse(input$cv_auc_slider == 0.5, "", paste0(",\n       AUC = ", input$cv_auc_slider)),
+                          ifelse(input$cv_method == "GLM", "", paste0(",\n       Method = ", "'", input$cv_method, "'")),
+                          ifelse(input$ntree != 500 && input$options == T, paste0(",\n       ntrees = ", input$ntree), ""),
+                          ifelse(!is.na(input$mtry) && input$options == T, paste0(",\n       mTry = ", input$mtry), ""),
+                          ifelse(input$svm_kernel != "linear" && input$options == T, paste0(",\n       Kernel = ", "'", input$svm_kernel, "'"), ""),
+                          ifelse(!is.na(input$svm_gamma) && input$options == T, paste0(",\n       Gamma = ", input$svm_gamma), ""),
+                          ifelse(!is.na(input$max_depth) && input$options == T, paste0(",\n       max.depth = ", input$max_depth), ""),
+                          ifelse(input$exclude_alg == "0" && length(input$exclude_alg) == 1, "",
+                                 paste0(",\n       exclude = c(", toString(paste0("'", input$exclude_alg[-which(input$exclude_alg %in% "0")])),")")),
+                          ifelse(is.na(input$seed) || input$seed_ask == F, "", paste0(",\n       seed = ", input$seed)),")")
+      
     } else if (input$model_type ==3 ){
-    # Nested Cross-fold Validation -------------------------------------------------------
+      # Nested Cross-fold Validation -------------------------------------------------------
       code_str <<- paste0("blkboxNCV(data = ", input$data_selection,
-                     ", labels = ", input$label_selection,
-                     ifelse(input$inner_folds == 10, "", paste0(",\n       folds = ", input$fold_number)),
-                     ifelse(input$outer_folds == 10, "", paste0(",\n       folds = ", input$fold_number)),
-                     ifelse(input$ncv_repeat_number == 1, "", paste0(",\n       repeats = ", input$ncv_repeat_number)),
-                     ifelse(input$ncv_auc_slider == 0.5, "", paste0(",\n       AUC = ", input$ncv_auc_slider)),
-                     ifelse(input$ncv_method == "GLM", "", paste0(",\n       Method = ", "'", input$ncv_method, "'")),
-                     ifelse(input$ntree != 500 && input$options == T, paste0(",\n       ntrees = ", input$ntree), ""),
-                     ifelse(!is.na(input$mtry) && input$options == T, paste0(",\n       mTry = ", input$mtry), ""),
-                     ifelse(input$svm_kernel != "linear" && input$options == T, paste0(",\n       Kernel = ", "'", input$svm_kernel, "'"), ""),
-                     ifelse(!is.na(input$svm_gamma) && input$options == T, paste0(",\n       Gamma = ", input$svm_gamma), ""),
-                     ifelse(!is.na(input$max_depth) && input$options == T, paste0(",\n       max.depth = ", input$max_depth), ""),
-                     ifelse(input$exclude_alg == "0" && length(input$exclude_alg) == 1, "",
-                            paste0(",\n       exclude = c(", toString(paste0("'", input$exclude_alg[-which(input$exclude_alg %in% "0")])),")")),
-                     ifelse(is.na(input$seed) || input$seed_ask == F, "", paste0(",\n       seed = ", input$seed)),")")
+                          ", labels = ", input$label_selection,
+                          ifelse(input$inner_folds == 10, "", paste0(",\n       folds = ", input$fold_number)),
+                          ifelse(input$outer_folds == 10, "", paste0(",\n       folds = ", input$fold_number)),
+                          ifelse(input$ncv_repeat_number == 1, "", paste0(",\n       repeats = ", input$ncv_repeat_number)),
+                          ifelse(input$ncv_auc_slider == 0.5, "", paste0(",\n       AUC = ", input$ncv_auc_slider)),
+                          ifelse(input$ncv_method == "GLM", "", paste0(",\n       Method = ", "'", input$ncv_method, "'")),
+                          ifelse(input$ntree != 500 && input$options == T, paste0(",\n       ntrees = ", input$ntree), ""),
+                          ifelse(!is.na(input$mtry) && input$options == T, paste0(",\n       mTry = ", input$mtry), ""),
+                          ifelse(input$svm_kernel != "linear" && input$options == T, paste0(",\n       Kernel = ", "'", input$svm_kernel, "'"), ""),
+                          ifelse(!is.na(input$svm_gamma) && input$options == T, paste0(",\n       Gamma = ", input$svm_gamma), ""),
+                          ifelse(!is.na(input$max_depth) && input$options == T, paste0(",\n       max.depth = ", input$max_depth), ""),
+                          ifelse(input$exclude_alg == "0" && length(input$exclude_alg) == 1, "",
+                                 paste0(",\n       exclude = c(", toString(paste0("'", input$exclude_alg[-which(input$exclude_alg %in% "0")])),")")),
+                          ifelse(is.na(input$seed) || input$seed_ask == F, "", paste0(",\n       seed = ", input$seed)),")")
     }
-
+    
     output$code <- renderText({code_str})
-
+    
   })
-
+  
+  observeEvent(input$viewpane_on, {
+    toggle("viewpane")
+    toggle("top_panel")
+    toggle("visualis_opts")
+    toggle("viewpane_on")
+    toggle("model_on")
+    toggle("get_code")
+    toggle("submit_model")
+    toggle("code_well")
+  })
+  
+  observeEvent(input$model_on, {
+    toggle("viewpane")
+    toggle("top_panel")
+    toggle("visualis_opts")
+    toggle("model_on")
+    toggle("viewpane_on")
+    toggle("get_code")
+    toggle("submit_model")
+  })
+  
+  
+  observeEvent(input$get_code, {
+    toggle("code_well")
+  })
+  
+  
   # Performance Output of Model -----------------------------------------------------------
   # Coming Soon
 
